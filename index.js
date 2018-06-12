7aa303107a498be8feac3dccc87cf49f3a46148d
Carson Kelley <carsonmkelley@gmail.com>
2018-06-12 17:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var theyPlay = []
   for(var i = 0; i < musicians.length; i++) {
-    
+    theyPlay.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+  return theyPlay
 }
